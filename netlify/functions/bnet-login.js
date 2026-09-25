df7b5661a8076c44077fc139ac5028d6a49ba913
Clay <clayvanderburg@gmail.com>
2026-09-24 19:31:07 -0500

    Show whether Battle.net credentials are visible to the login function.

--- netlify/functions/bnet-login.js
+++ netlify/functions/bnet-login.js
@@ -1,7 +1,18 @@
 const crypto = require('crypto');
 const { bnetConfigured, redirectUri, stateCookie } = require('./player-session');
 
-exports.handler = async () => {
+exports.handler = async (event) => {
+  if (event.queryStringParameters?.debug === '1') {
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json', 'Cache-Control': 'no-store' },
+      body: JSON.stringify({
+        id: Boolean(process.env.BNET_CLIENT_ID),
+        secret: Boolean(process.env.BNET_CLIENT_SECRET),
+        redirect: Boolean(process.env.BNET_REDIRECT_URI)
+      })
+    };
+  }
   if (!bnetConfigured()) {
     return {
       statusCode: 501,
